236564ac32d97be91fa961659318e8d280cf1736
Marti Pericay <marti.pericay@gmail.com>
2018-08-30 16:08:21 +0200

    better results list and no results

--- js/modules/ui.js
+++ js/modules/ui.js
@@ -13,25 +13,33 @@ define(['search', 'bootstrap', 'typeahead'], function(search) {
           var results = search.search($("#proj").val());
           $("#resultsList").empty();
           //if results
-          for(var i=0; i < results.length; i++) {
-            var proj = search.getProject(results[i].ref);
-            $("#resultsList").append(buildSearchResult(proj));
-          };
+          if (!results.length) {
+            $("#noResults").html("No s'han trobat resultats");
+          } else {
+            $("#noResults").html("Mostrant " + results.length + " resultats");
+            for(var i=0; i < results.length; i++) {
+              var proj = search.getProject(results[i].ref);
+              $("#resultsList").append(buildSearchResult(proj));
+            };
+          }
         });
-      
+    
 	var buildSearchResult = function (doc) {
-	  var li = document.createElement('li'),
-	      h2 = document.createElement('h2'),
-	      p = document.createElement('p')
-
-	  h2.dataset.field = 'name'
-	  h2.textContent = doc.properties.nom_del_projecte
-
-	  p.dataset.field = 'body'
-	  p.textContent = doc.properties.web_del_projecte
-
-	  li.appendChild(h2)
-	  li.appendChild(p)
+        var li = document.createElement('li'),
+            img = document.createElement('img'),
+            h2 = document.createElement('h2'),
+            p = document.createElement('p')
+        
+        h2.textContent = doc.properties.nom_del_projecte;
+        h2.className = "searchTitle";
+        img.setAttribute("src", "img/projects/example.jpg");
+        img.className = "searchPic";
+        p.textContent = doc.properties.ambit_geografic;
+        p.className = "searchSubtitle";
+  
+        li.appendChild(h2);
+        li.appendChild(img);
+        li.appendChild(p);
 
 	  return li
 	}    
