990f92ebd236b457b05dc2acd87aff146e2edd52
Marti Pericay <marti.pericay@gmail.com>
2018-09-27 12:09:06 +0200

    logos correctes al footer

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 		<div class="footer">
 			<p>Si no s’indica el contrari els continguts del lloc web Ciència Ciutadana es publiquen sota una llicència
 			Creative Commons Attribution 4.0 International (<a href="http://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>)</p>
-			<p>Consorci format per 
-			<img class="logos" src="img/logos/mnat.png" alt="MCNB logo" id="mcnbLogo"></p>
+			<p>
+			<!--Consorci format per-->
+			<img src="img/logos/consorci.png" alt="Consorci logo" id="consorciLogo"></p>
 		</div>
