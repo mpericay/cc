75f00bd67ab311724100d9fee4378c4ac7da50e1
Marti Pericay <marti.pericay@gmail.com>
2018-10-03 11:40:53 +0200

    Edició pàgina recursos

--- contingut/recursos.php
+++ contingut/recursos.php
@@ -1,16 +1,18 @@
-<h2>Recursos / Plataformes</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus iaculis metus ultricies malesuada. Vivamus fermentum, sem id ullamcorper volutpat, diam massa fringilla justo, vel gravida sem lectus at eros. Phasellus viverra lorem sit amet urna viverra, ut sagittis sem vehicula. Donec gravida finibus diam, quis bibendum augue bibendum non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla imperdiet posuere turpis, eget rutrum elit gravida eu. Maecenas quis porttitor ipsum. Cras pharetra turpis eu elit gravida, eu ullamcorper lacus faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vitae arcu odio. Nullam et placerat nisi.</p>
-<h4>Recursos</h4>
-<ul>
-    <li>Recurs 1: <a href="#">www.biodiversitat.com</a></li>
-    <li>Recurs 2: <a href="#">www.natura.com</a></li>
-    <li>Recurs 3: <a href="#">www.gbif.com</a></li>
-</ul>
-
-<p>Integer vel eros feugiat, lobortis ipsum vitae, consequat neque. Fusce in diam urna. Sed non nunc ac dolor condimentum tristique. Quisque sollicitudin, metus in fringilla condimentum, orci nisl luctus lorem, eget laoreet nibh nunc vel enim. Etiam pharetra mi ligula, ac semper ex lacinia ac. Duis lobortis volutpat libero. Mauris fringilla felis ac elit ultricies, malesuada molestie velit fringilla. Nulla fringilla massa sit amet dolor auctor, vitae feugiat mauris luctus. Etiam iaculis porttitor nisi vitae tincidunt. Proin dictum fringilla dolor lobortis egestas</p>
-
-<h4>Plataformes</h4>
-<ul>
-    <li>Plataforma 1: <a href="#">www.biodiversitat.com</a></li>
-    <li>Plataforma 2: <a href="#">www.natura.com</a></li>
-</ul>
+<div class="container">
+    <h2>Recursos / Plataformes</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus iaculis metus ultricies malesuada. Vivamus fermentum, sem id ullamcorper volutpat, diam massa fringilla justo, vel gravida sem lectus at eros. Phasellus viverra lorem sit amet urna viverra, ut sagittis sem vehicula. Donec gravida finibus diam, quis bibendum augue bibendum non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla imperdiet posuere turpis, eget rutrum elit gravida eu. Maecenas quis porttitor ipsum. Cras pharetra turpis eu elit gravida, eu ullamcorper lacus faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vitae arcu odio. Nullam et placerat nisi.</p>
+    <h4>Recursos</h4>
+    <ul>
+        <li>Recurs 1: <a href="#">www.biodiversitat.com</a></li>
+        <li>Recurs 2: <a href="#">www.natura.com</a></li>
+        <li>Recurs 3: <a href="#">www.gbif.com</a></li>
+    </ul>
+    
+    <p>Integer vel eros feugiat, lobortis ipsum vitae, consequat neque. Fusce in diam urna. Sed non nunc ac dolor condimentum tristique. Quisque sollicitudin, metus in fringilla condimentum, orci nisl luctus lorem, eget laoreet nibh nunc vel enim. Etiam pharetra mi ligula, ac semper ex lacinia ac. Duis lobortis volutpat libero. Mauris fringilla felis ac elit ultricies, malesuada molestie velit fringilla. Nulla fringilla massa sit amet dolor auctor, vitae feugiat mauris luctus. Etiam iaculis porttitor nisi vitae tincidunt. Proin dictum fringilla dolor lobortis egestas</p>
+    
+    <h4>Plataformes</h4>
+    <ul>
+        <li>Plataforma 1: <a href="#">www.biodiversitat.com</a></li>
+        <li>Plataforma 2: <a href="#">www.natura.com</a></li>
+    </ul>
+</div>
