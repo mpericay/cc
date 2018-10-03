e749393a66967b7173aa6099ab81d1f2b9d2a2bf
Marti Pericay <marti.pericay@gmail.com>
2018-10-03 11:40:40 +0200

    Edició pàgina contacte

--- contingut/contacte.php
+++ contingut/contacte.php
@@ -1,3 +1,7 @@
-<h2>Contacte</h2>
-<h4>Vols afegir un projecte?</h4>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus iaculis metus ultricies malesuada. Vivamus fermentum, sem id ullamcorper volutpat, diam massa fringilla justo, vel gravida sem lectus at eros. Phasellus viverra lorem sit amet urna viverra, ut sagittis sem vehicula. Donec gravida finibus diam, quis bibendum augue bibendum non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla imperdiet posuere turpis, eget rutrum elit gravida eu. Maecenas quis porttitor ipsum. Cras pharetra turpis eu elit gravida, eu ullamcorper lacus faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vitae arcu odio. Nullam et placerat nisi.</p>
+<div class="container">
+    <h2>Contacte</h2>
+    <h4>Trobes a faltar un projecte?</h4>
+    <p>Si treballes en algun projecte d'estudi de la diversitat natural que no trobes aquí, no dubtis a contactar amb nosaltres
+    i enviar-nos les següents dades: nom del projecte, una fotografia, una breu descripció, l'activitat prevista, l'àmbit geogràfic i
+    biològic i adreces web o de xarxes socials a nouprojecte@cienciaciutadana.org.</p>
+</div>
