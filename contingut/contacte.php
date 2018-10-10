5f61a5cedf178c2cc286a9d268fc3a4009f690c1
Marti Pericay <marti.pericay@gmail.com>
2018-10-10 17:58:55 +0200

    Editar pàgina contacte

--- contingut/contacte.php
+++ contingut/contacte.php
@@ -4,4 +4,6 @@
     <p>Si treballes en algun projecte d'estudi de la diversitat natural que no trobes aquí, no dubtis a contactar amb nosaltres
     i enviar-nos les següents dades: nom del projecte, una fotografia, una breu descripció, l'activitat prevista, l'àmbit geogràfic i
     biològic i adreces web o de xarxes socials a nouprojecte@cienciaciutadana.org.</p>
+    <p>Actualment comptem amb més de 100 projectes, repartits entre Catalunya i les Illes Balears. Si en trobes a faltar algun al cercador, no dubtis a contactar amb nosaltres.</p>
+    <img src="img/mapa.jpg" style='width: 500px;'/>
 </div>
