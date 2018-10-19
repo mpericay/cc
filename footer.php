743b5291df37a0b60f280040c38ac0cd9394be73
Marti Pericay <marti.pericay@gmail.com>
2018-10-19 13:42:03 +0200

    afegir Montse als crèdits

--- footer.php
+++ footer.php
@@ -5,5 +5,5 @@
 			<!--Consorci format per-->
 			El Museu de Ciències Naturals de Barcelona és un consorci format per l’Ajuntament de Barcelona i la Generalitat de Catalunya</p>
 			<p>
-			Web desenvolupat per <a href="http://www.pericay.com">www.pericay.com</a> i <a href="http://www.marccots.com">www.marccots.com</a></p>
+			Web desenvolupat per <a href="http://www.pericay.com">www.pericay.com</a> i <a href="http://www.marccots.com">www.marccots.com</a>. Disseny exposició per part de <a href="http://www.pfpdisseny.com">Montse Fabregat</a></p>
 		</div>
