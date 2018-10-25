ba1a8ce2c4e7dc46574492e9d643c704b937d401
Marti Pericay <marti.pericay@gmail.com>
2018-10-25 12:06:59 +0200

    Revert "Custom selects del buscador"
    This reverts commit d1735934201df8a242c93132db6f3fcf3a88e836.

--- index.php
+++ index.php
@@ -44,27 +44,24 @@
 						<div class="col-lg-6 col-lg-offset-0 col-md-8 col-md-offset-2">
 				        	<form class="form-inline" role="search">
 								<div class="form-group" id="searchProj"><input id="proj" class="form-input" placeholder="Projecte"></div>
-							  	<div id="searchBio" class="custom-select form-group">
-								  <select>
-								    <option value="Ambiental">Ambiental</option>
-									<option value="Aus">Aus</option>
-									<option value="Coleòpters">Coleòpters</option>
-									<option value="General">General</option>
-									<option value="Líquens">Líquens</option>
-								  </select>
-								</div>
+								<div class="form-group" id="searchBio"><input list="ambits" id="bio" class="form-input" placeholder="Àmbit biològic"></div>
+								  <datalist id="ambits">
+									<option value="Ambiental">
+									<option value="Aus">
+									<option value="Coleòpters">
+									<option value="General">
+									<option value="Líquens">
+								  </datalist>
 								<div class="form-group" id="searchLoc"><input id="loc" class="form-input" placeholder="Localització"></div>
-								<div id="searchStatus"class="custom-select form-group">
-								 <select id="oberttancat">
-									<option value="Obert">Obert</option>
-									<option value="Tancat">Tancat</option>
-								  </select>
-								</div>
+								<div class="form-group" id="searchStatus"><input list="oberttancat" id="actiu" class="form-input" placeholder="Obert/Tancat"></div>
+								<datalist id="oberttancat">
+									<option value="Obert">
+									<option value="Tancat">
+								  </datalist>
 								<input type="button" id="searchBtn" class="boto" value="Buscar"></input>
 								<input id="clearBtn" class="boto" type="reset" value="Netejar"></input>
 							</form>
 						</div>
-
 					</div>
 				</div>
 			</div>
@@ -131,81 +128,6 @@
 	
 	<script src="js/config.js"></script>
 	<script src="js/lib/require.js" data-main="ui"></script>
-
-	<script type="text/javascript">
-		var x, i, j, selElmnt, a, b, c;
-		/*look for any elements with the class "custom-select":*/
-		x = document.getElementsByClassName("custom-select");
-		for (i = 0; i < x.length; i++) {
-		  selElmnt = x[i].getElementsByTagName("select")[0];
-		  /*for each element, create a new DIV that will act as the selected item:*/
-		  a = document.createElement("DIV");
-		  a.setAttribute("class", "select-selected form-input");
-		  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-		  x[i].appendChild(a);
-		  /*for each element, create a new DIV that will contain the option list:*/
-		  b = document.createElement("DIV");
-		  b.setAttribute("class", "select-items select-hide");
-		  for (j = 0; j < selElmnt.length; j++) {
-		    /*for each option in the original select element,
-		    create a new DIV that will act as an option item:*/
-		    c = document.createElement("DIV");
-		    c.innerHTML = selElmnt.options[j].innerHTML;
-		    c.addEventListener("click", function(e) {
-		        /*when an item is clicked, update the original select box,
-		        and the selected item:*/
-		        var y, i, k, s, h;
-		        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-		        h = this.parentNode.previousSibling;
-		        for (i = 0; i < s.length; i++) {
-		          if (s.options[i].innerHTML == this.innerHTML) {
-		            s.selectedIndex = i;
-		            h.innerHTML = this.innerHTML;
-		            y = this.parentNode.getElementsByClassName("same-as-selected");
-		            for (k = 0; k < y.length; k++) {
-		              y[k].removeAttribute("class");
-		            }
-		            this.setAttribute("class", "same-as-selected");
-		            break;
-		          }
-		        }
-		        h.click();
-		    });
-		    b.appendChild(c);
-		  }
-		  x[i].appendChild(b);
-		  a.addEventListener("click", function(e) {
-		      /*when the select box is clicked, close any other select boxes,
-		      and open/close the current select box:*/
-		      e.stopPropagation();
-		      closeAllSelect(this);
-		      this.nextSibling.classList.toggle("select-hide");
-		      this.classList.toggle("select-arrow-active");
-		    });
-		}
-		function closeAllSelect(elmnt) {
-		  /*a function that will close all select boxes in the document,
-		  except the current select box:*/
-		  var x, y, i, arrNo = [];
-		  x = document.getElementsByClassName("select-items");
-		  y = document.getElementsByClassName("select-selected");
-		  for (i = 0; i < y.length; i++) {
-		    if (elmnt == y[i]) {
-		      arrNo.push(i)
-		    } else {
-		      y[i].classList.remove("select-arrow-active");
-		    }
-		  }
-		  for (i = 0; i < x.length; i++) {
-		    if (arrNo.indexOf(i)) {
-		      x[i].classList.add("select-hide");
-		    }
-		  }
-		}
-		/*if the user clicks anywhere outside the select box,
-		then close all select boxes:*/
-		document.addEventListener("click", closeAllSelect);
-	</script>
     
 </body>
 
