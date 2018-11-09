62b464e81beb8c10c74bff76a38f029e22635003
Marti Pericay <marti.pericay@gmail.com>
2018-11-09 14:23:52 +0100

    nou recurs: SamplEase

--- contingut/recursos.php
+++ contingut/recursos.php
@@ -5,6 +5,7 @@
         <li>iNaturalist: <a href="https://www.inaturalist.org">https://www.inaturalist.org</a></li>
         <li>Natusfera: <a href="http://natusfera.gbif.es">http://natusfera.gbif.es</a></li>
         <li>ZamiaDroid: <a href="http://biodiver.bio.ub.es/zamiaDroid/index.jsp">http://biodiver.bio.ub.es/zamiaDroid/index.jsp</a></li>
+        <li>SamplEase: <a href="https://onlinelibrary.wiley.com/doi/10.1002/ece3.4503">https://onlinelibrary.wiley.com/doi/10.1002/ece3.4503</a></li>
     </ul>
       
     <h4>Plataformes: espais on s’agreguen diversos projectes de ciència ciutadana per a l’estudi de la biodiversitat</h4>
