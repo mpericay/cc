5993d527d9c7592d3edd6852a31667e9d132b4e7
marccots <hey@marccots.com>
2018-11-12 17:08:55 +0100

    Arreglar slogan header

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
 		<div class="header">
+			<div class="slogan-resp"><h2>Fem Natura, Som Natura</h2></div> 
 			<div class="logos-left">
 			 	<a href=""><img class="logos" src="img/logos/mnat.png" alt="MCNB logo" id="mcnbLogo"></a>
 			</div>
-			<h2>Fem Natura, Som Natura</h2>
+			<div class="slogan"><h2>Fem Natura, <br>Som Natura</h2></div>
 			<div class="logos-right">
 			  	<a href=""><img class="logos" src="img/logos/gene.png" alt="Generalitat logo" id="geneLogo"></a>
-			</div>   
+			</div>
 		</div>
 
       
